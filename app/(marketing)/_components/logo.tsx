20411ccdfb3fcd90c6e6d513557fb1d1b3c9aa7a
Imtiaz Uddin <iuashrafi@gmail.com>
2023-12-30 22:42:48 +0530

    formatted using prettier

--- app/(marketing)/_components/logo.tsx
+++ app/(marketing)/_components/logo.tsx
@@ -1,15 +1,14 @@
 import Image from "next/image";
 import { Poppins } from "next/font/google";
-
-
 import { cn } from "@/lib/utils";
 
-const font = Poppins({ 
-    subsets: ['latin'],
-    weight:['400', '600']
-})
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "600"],
+});
 export const Logo = () => {
-    return <div className="hidden md:flex items-center gap-x-2">
+  return (
+    <div className="hidden md:flex items-center gap-x-2">
       <Image
         src="/logo.svg"
         height="40"
@@ -24,8 +23,7 @@ export const Logo = () => {
         alt="Logo"
         className="hidden dark:block"
       />
-      <p className={cn("font-semibold", font.className)}>
-        Jotion
-      </p>
+      <p className={cn("font-semibold", font.className)}>Jotion</p>
     </div>
-}
+  );
+};
--- hooks/use-scroll-top.tsx
+++ hooks/use-scroll-top.tsx
@@ -1,21 +1,18 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 export const useScrollTop = (threshold = 10) => {
-    const [scrolled, setScrolled] = useState(false);
-    useEffect(() => {
-        const handleScroll = () => {
-            if (window.scrollY > threshold) {
-                setScrolled(true);
-            }
-            else {
-               setScrolled(false)
-            }
-        }
-        window.addEventListener('scroll', handleScroll);
-        return ()=>window.removeEventListener('scroll', handleScroll);
-    }, [threshold]);
+  const [scrolled, setScrolled] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > threshold) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [threshold]);
 
-    return scrolled; 
-}
-
-
+  return scrolled;
+};
