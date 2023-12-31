aad21c430d234d65a88fda0d5f4c20ef1026572a
Imtiaz Uddin <iuashrafi@gmail.com>
2023-12-31 20:22:31 +0530

    authorization and sidebar

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import Image from "next/image";
+
+const DocumentsPage = () => {
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dark.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="hidden dark:block"
+      />
+    </div>
+  );
+};
+
+export default DocumentsPage;
